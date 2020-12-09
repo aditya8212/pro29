e41a06e618d25070bd6568d6ecc2c979045c2133
aditya8212 <73173896+aditya8212@users.noreply.github.com>
2020-12-09 18:22:35 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,22 @@
+class Box {
+    constructor(x, y) {
+      var options = {
+          isStatic:true,
+         
+          density:1.0
+      }
+      this.body = Bodies.rectangle(x,y,20,40,options);
+      this.width = 25;
+      this.height = 30;
+      
+      World.add(world,this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      
+      rectMode(CENTER);
+      stroke("aquamarine");
+      rect(pos.x, pos.y, this.width, this.height);
+      
+    }
+  }
